f7d29e2a02e0fec165e673b0166a4c97ec10a1f7
abdallah-essam <abdallah.esam.eldin@gmail.com>
2023-06-17 11:03:04 +0300

    on board desing

--- lib/modules/splash/presentation/screens/splash_screen.dart
+++ lib/modules/splash/presentation/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:ecommerce/shared/constants/colors/my_colors.dart';
 import 'package:ecommerce/shared/constants/images/my_images.dart';
 import 'package:ecommerce/shared/routes/my_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -23,6 +24,13 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColors.splashBackground,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 0,
+        systemOverlayStyle:
+            const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+      ),
       body: Center(
         child: Image.asset(MyImages.splashLogo, height: 43, width: 170),
       ),
